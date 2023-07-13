156bf786e8423128b8270f9ddd7d5c8722b252ff
amrithasv <svamritha93@gmail.com>
2023-07-13 06:29:16 +0000

    commit

--- huffman.py
+++ huffman.py
@@ -77,3 +77,46 @@ while len(nodes) > 1:
  
 # Huffman Tree is ready!
 printNodes(nodes[0])
+def decode_text(root, encoded_text):
+    decoded_text = ""
+    current_node = root
+
+    for bit in encoded_text:
+        if bit == "0":
+            current_node = current_node.left
+        else:
+            current_node = current_node.right
+
+        if current_node.left is None and current_node.right is None:
+            decoded_text += current_node.symbol
+            current_node = root
+
+    return decoded_text
+encoded_text = (input("enter the code:"))
+decoded_text = decode_text(nodes[0], encoded_text)
+print("Decoded text:", decoded_text)
+
+
+def encode_text(root, text):
+    huffman_codes = {}
+
+    def generate_codes(node, current_code=""):
+        if node is None:
+            return
+
+        if node.symbol:
+            huffman_codes[node.symbol] = current_code
+
+        generate_codes(node.left, current_code + "0")
+        generate_codes(node.right, current_code + "1")
+
+    generate_codes(root)
+
+    encoded_text = ""
+    for char in text:
+        encoded_text += huffman_codes[char]
+
+    return encoded_text
+text=input("Enter text:")
+encoded_text = encode_text(nodes[0], text)
+print("Encoded text:", encoded_text)
