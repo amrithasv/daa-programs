915d94cd3d11841c80b31db5a628ced1acf04113
amrithasv <svamritha93@gmail.com>
2023-07-27 05:35:24 +0000

    commit

--- dijkstra.py
+++ dijkstra.py
@@ -1,58 +1,81 @@
 import heapq
-def dijsktra(graph,start):
-   
-    distances={node:float('inf') for node in graph}
-    distances[start]=0
-    heap=[(0,start)]
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    heap = [(0, start)]
+
     while heap:
-        current_dist,current_node=heapq.heappop(heap)
-        if current_dist>distances[current_node]:
+        current_dist, current_node = heapq.heappop(heap)
+        if current_dist > distances[current_node]:
             continue
-        for neighbor,weight in graph[current_node].items():
-            distance=current_dist+weight
-            if distance<distances[neighbor]:
-                distances[neighbor]=distance
-                heapq.heappush(heap,(distance,neighbor))
+        for neighbor, weight in graph[current_node].items():
+            distance = current_dist + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(heap, (distance, neighbor))
+
     return distances
-def find_optimal_route(graph,start,destination):
-    distances= dijsktra(graph,start)
-    if distances[destination]==float('inf'):
+
+def find_optimal_route(graph, start, destination):
+    distances = dijkstra(graph, start)
+    if distances[destination] == float('inf'):
         return None
-    route=[]
-    node=destination
-    while node!=start:
+    route = []
+    node = destination
+
+    while node != start:
         route.append(node)
-        neighbors=graph[node]
-        min_distance=float('inf')
-        next_node=None
-        for neighbor,weight in neighbors.item():
-            if distances[neighbor]+weight==distances[node]and distances[neighbor]<min_distance:
-                min_distance=distances[neighbor]
-                next_node=neighbor
-                if next_node is None or next_node in route:
-                    return None
-                node=next_node
+        neighbors = graph[node]
+        min_distance = float('inf')
+        next_node = None
+        for neighbor, weight in neighbors.items():
+            if distances[neighbor] + weight == distances[node] and distances[neighbor] < min_distance:
+                min_distance = distances[neighbor]
+                next_node = neighbor
+        if next_node is None or next_node in route:
+            return None
+        node = next_node
+
     route.append(start)
     route.reverse()
     return route
-graph={
-    'A':{'B':3,'C':99,'D':7,'E':99},
-    'B':{'A':3,'C':4,'D':2,'E':99},
-    'C':{'A':99,'B':4,'D':5,'E':6},
-    'D':{'A':7,'B':2,'C':5,'E':4},
-    'E':{'A':99,'B':99,'C':6 ,'D':4},
 
+# Get user input for the graph
+graph = {}
+num_edges = int(input("Enter the number of edges in the graph: "))
+print("Enter the edges in the format 'node1 node2 weight':")
+for _ in range(num_edges):
+    edge = input().split()
+    node1, node2, weight = edge[0], edge[1], int(edge[2])
+    if node1 not in graph:
+        graph[node1] = {}
+    if node2 not in graph:
+        graph[node2] = {}
+    graph[node1][node2] = weight
+    graph[node2][node1] = weight
 
-}
-start_location='A'
-destination_loaction='E'
-optimal_route=find_optimal_route(graph,start_location,destination_loaction)
-if optimal_route is None:
-    print("No valid route from start to dest")
-else:print("optimal route:",'->'.join(optimal_route))
+# Get user input for the start and destination nodes
+start_node = input("Enter the start node: ")
+destination_node = input("Enter the destination node: ")
 
+# Find the optimal route
+route = find_optimal_route(graph, start_node, destination_node)
 
-
-    
-
-
+# Print the result
+if route is None:
+    print("No route found.")
+else:
+    print("Optimal route:", route)
+    #Enter the number of edges in the graph: 7
+#Enter the edges in the format 'node1 node2 weight':
+#a b 3
+#b c 4
+#c d 5
+#d e 4
+#c e 6
+#b d 2
+#a d 7
+#Enter the start node: a
+#Enter the destination node: e
+#Optimal route: ['a', 'b', 'd', 'e']
